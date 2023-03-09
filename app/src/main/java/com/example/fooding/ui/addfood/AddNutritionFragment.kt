c76ff0152face75fc65326a6988599e56910e17f
JayBadass <jaybadass@naver.com>
2023-03-09 16:39:58 +0900

    [feat] AddNutritionFragment : 생성 및 바인딩, 추가하기 버튼 클릭 시 데이터 저장/AddFoodFragment에 데이터 전달/종료 구현

---
+++ app/src/main/java/com/example/fooding/ui/addfood/AddNutritionFragment.kt
@@ -0,0 +1,50 @@
+package com.example.fooding.ui.addfood
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.fooding.databinding.FragmentAddnutritionBinding
+
+class AddNutritionFragment: Fragment() {
+
+    private var _binding: FragmentAddnutritionBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: AddNutritionViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddnutritionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addNutritionViewModel = viewModel
+        setAddButton()
+    }
+
+    private fun setAddButton() {
+        binding.btnAdd.setOnClickListener {
+            viewModel.serializeNutrition()
+            val data = viewModel.foodName.value
+            val nutrition = viewModel.nutrition
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("nutritionList", nutrition)
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("foodName", data)
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun setSearchButton() {
+        binding.btnSearch.setOnClickListener {
+//            Search Fragment 이동
+        }
+    }
+}
