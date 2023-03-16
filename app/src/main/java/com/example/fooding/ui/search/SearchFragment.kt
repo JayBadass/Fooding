b513ff61ad9c73285c4743b878ca8f49405b5931
JayBadass <jaybadass@naver.com>
2023-03-16 14:09:31 +0900

    [feat] SearchFragment.kt: viewModel 연결/recyclerView 연결/검색버튼 구현/프로그래스바 구현

--- app/src/main/java/com/example/fooding/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/fooding/ui/search/SearchFragment.kt
@@ -1,16 +1,31 @@
 package com.example.fooding.ui.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fooding.FoodingApplication
+import com.example.fooding.data.model.Nutrition
+import com.example.fooding.data.source.SearchRepository
 import com.example.fooding.databinding.FragmentSearchBinding
+import com.example.fooding.ui.common.FoodClickListener
+import kotlinx.coroutines.launch
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), FoodClickListener {
 
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: SearchViewModel by viewModels {
+        SearchViewModel.provideFactory(
+            repository = SearchRepository(FoodingApplication.appContainer.provideFoodApiClient())
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +34,44 @@ class SearchFragment : Fragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.searchViewModel = viewModel
+        setLayout()
+        setSearchButton()
+    }
+
+    private fun setLayout() {
+        val adapter = FoodListAdapter(this)
+        binding.rvFoodList.adapter = adapter
+        binding.rvFoodList.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun setSearchButton() {
+        binding.btnSearch.setOnClickListener {
+            lifecycleScope.launch {
+                showLoadingIndicator()
+                val foodList = viewModel.getFoodList()
+                Log.d("fragment", "$foodList")
+                if (foodList != null) {
+                    (binding.rvFoodList.adapter as FoodListAdapter).submitList(foodList)
+                }
+                hideLoadingIndicator()
+            }
+        }
+    }
+
+    private fun showLoadingIndicator() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoadingIndicator() {
+        binding.progressBar.visibility = View.GONE
+    }
+
+    override fun onClick(nutrition: Nutrition) {
+        TODO("Not yet implemented")
     }
 }
