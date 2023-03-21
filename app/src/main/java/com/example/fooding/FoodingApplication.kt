e8910a0cd9591fc3826093f2f06fd11de0de6dbb
JayBadass <jaybadass@naver.com>
2023-03-21 11:12:02 +0900

    [feat] FoodingApplication.kt: application에 appdatabase 추가

--- app/src/main/java/com/example/fooding/FoodingApplication.kt
+++ app/src/main/java/com/example/fooding/FoodingApplication.kt
@@ -1,15 +1,18 @@
 package com.example.fooding
 
 import android.app.Application
+import com.example.fooding.data.source.local.AppDatabase
 
 class FoodingApplication : Application() {
 
     companion object {
         lateinit var appContainer: AppContainer
+        lateinit var database: AppDatabase
     }
 
     override fun onCreate() {
         super.onCreate()
         appContainer = AppContainer()
+        database = AppDatabase.getInstance(this)
     }
 }
